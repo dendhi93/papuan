a1849b9b843759b5ed4153f7dea76869e27bd225
dendhi93 <dendhi.fendi@gmail.com>
2019-10-01 15:21:01 +0700

    add butterknife

--- app/src/main/java/com/dracoo/papuamobile/preparation/LoginActivity.java
+++ app/src/main/java/com/dracoo/papuamobile/preparation/LoginActivity.java
@@ -5,21 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.dracoo.papuamobile.R;
 import com.dracoo.papuamobile.Utils.SharedPrefManager;
 import com.dracoo.papuamobile.feature.Home.HomeActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ProgressDialog pDialog;
     private String ResultWS;
     private SharedPrefManager sharedPrefManager;
+    @BindView(R.id.etUserName)
+    EditText etUsername;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
 
     @Override
